153be10ba36a4a7380c7b25a4684185494c0bda7
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-11-13 07:32:38 +0700

    support heroku host

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const { WebhookClient, MessageEmbed } = require("discord.js")
 const config = require("./config.json")
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+app.get("/", (req, res) => {
+    res.send("A fake server to fool Heroku")
+})
+app.listen(port, "0.0.0.0", function () {
+    console.log(`Server listening on port ${port}\n`)
+})
 const hook = new WebhookClient({ url: config.webhookurl })
 const fetch = require("node-fetch")
 const radom = [
